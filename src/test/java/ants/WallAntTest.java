28f21c6992b63bdee58a49605046cfaccc986c06
muhammad hassan <hassanprofwor@gmail.com>
2026-04-26 17:50:04 +0100

    feat: DEV-35 - As a player, I want WallAnt so I can defend against bees longer.

---
+++ src/test/java/ants/WallAntTest.java
@@ -0,0 +1,49 @@
+package ants;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import core.AntColony;
+
+class WallAntTest
+{
+    @Test
+    void wallAntHasFoodCostOfFour()
+    {
+        WallAnt ant = new WallAnt();
+        assertEquals(4, ant.getFoodCost());
+    }
+
+    @Test
+    void wallAntHasArmorOfFour()
+    {
+        WallAnt ant = new WallAnt();
+        assertEquals(4, ant.getArmor());
+    }
+
+    @Test
+    void wallAntActionDoesNothing()
+    {
+        AntColony colony = new AntColony(1, 3, 0, 10);
+        WallAnt ant = new WallAnt();
+        int foodBefore = colony.getFood();
+
+        ant.action(colony);
+
+        // food should not change since WallAnt does nothing
+        assertEquals(foodBefore, colony.getFood());
+    }
+
+    @Test
+    void wallAntCanBeDeployed()
+    {
+        AntColony colony = new AntColony(1, 3, 0, 10);
+        WallAnt ant = new WallAnt();
+
+        colony.deployAnt(colony.getPlaces()[0], ant);
+
+        assertNotNull(colony.getPlaces()[0].getAnt());
+    }
+}
